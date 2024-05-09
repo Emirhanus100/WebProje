511b89a5e546ea43a2845d13bca6098e264dae39
Emirhanus100 <169179838+Emirhanus100@users.noreply.github.com>
2024-05-10 00:48:10 +0300

    değişiklik ilgialanı,mrias

---
+++ Mmirsaımız.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<head>
+
+<title>Hakkında Ekrani</title>
+<link rel="stylesheet" type="text/css" href="Mmirasımız.css">
+<script src="script.js" defer></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="BAŞLIK">
+        <div class="LOGOTASARIM">
+        <p>MİTA .</p>
+        </div>
+        <div class="Menü">
+            <span class="icon"><ion-icon name="menu"></ion-icon></span>
+            <ul>
+                <li><a href="LoginEkranı.php" class="link active">ANA SAYFA</a></li>
+                <li><a href="Hakkında.php" class="link active">HAKKINDA</a></li>
+                <li><a href="İlgialanı.php" class="link active">İLGİ ALANI</a></li>
+                <li><a href="Mmirsaımız.php" class="link active">MALATYA'NIN MİRASI</a>
+            </ul>
+        </div>
+    </div>
+<div class="HakAna">
+    <input type="checkbox" id="chk" aria-hidden="true"> 
+    <div class="Hakkındaİçerik">
+    <h1 align="center">ARSLANTEPE  HÖYÜĞÜ</h1>   
+    <div class="container">  
+        <br><br>      
+        <table>
+    <tr>
+      <td valign="top">
+        <h1>Arslantepe Höyüğü: Anadolu'nun Tarihine Pencere</h1><br>
+        <p>Malatya'da Fırat Nehri'nin kıyısında yer alan Arslantepe Höyüğü, M.Ö. 6000'den M.S. 11. yüzyıla kadar uzanan farklı</p> medeniyetlerin izlerini taşıyan, 
+        Türkiye'nin en büyük ve UNESCO Dünya Mirası Listesi'nde yer alan höyüklerinden biridir.</p> Saray, tapınak, evler, mezarlar ve 
+                çok sayıda eserin ortaya çıkarıldığı kazılar, bölgenin aristokrasinin doğduğu ve ilk devlet </p> şeklinin ortaya çıktığı resmi, dini 
+                    ve kültürel bir merkez olduğunu gösteriyor. Arslantepe Aslan Heykeli, Arslantepe Kralı </p>Mühürü ve Karahöyük Sfenksi gibi eserler, höyüğün önemini 
+                    vurgularken, Arslantepe Açık Hava Müzesi ve kazılardan</p> çıkan eserler ziyaretçilere Anadolu'nun tarihi ve kültürü hakkında eşsiz bir deneyim sunuyor.</p>
+      </td>
+      <td>
+      <br><br><br><br>
+        <figure>
+        <img src="Aslantepe-Hoyugu-.jpg" alt="Arslantepe Höyüğü Görseli"><figcaption>Dünyanın en eski kral mezarını bulunduran Arslantepe Höyüğü</figcaption>
+</figure>
+    </td>  
+    </tr>
+  </table>
+  <tr><br><br>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3127.6068671967114!2d38.35835927563708!3d38.381215071838874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x407636ec69a712a5%3A0x6e6aa35c7d317665!2zQXJzbGFudGVwZSBIw7Z5w7zEn8O8!5e0!3m2!1str!2str!4v1715287519446!5m2!1str!2str" width="700" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+  </iframe>
+</tr>
+</div>
+</div>
---
+++ İlgialanı.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<head>
+
+<title>Hakkında Ekrani</title>
+<link rel="stylesheet" type="text/css" href="ilgialanı.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="BAŞLIK">
+        <div class="LOGOTASARIM">
+        <p>MİTA .</p>
+        </div>
+        <div class="Menü">
+            <span class="icon"><ion-icon name="menu"></ion-icon></span>
+            <ul>
+                <li><a href="LoginEkranı.php" class="link active">ANA SAYFA</a></li>
+                <li><a href="Hakkında.php" class="link active">HAKKINDA</a></li>
+                <li><a href="İlgialanı.php" class="link active">İLGİ ALANI</a></li>
+                <li><a href="Mmirsaımız.php" class="link active">MALATYA'NIN MİRASI</a>
+            </ul>
+        </div>
+    </div>
+<div class="HakAna">
+    <input type="checkbox" id="chk" aria-hidden="true"> 
+    <div class="Hakkındaİçerik">
+    <div class="container">  
+        <br>
+        <br>
+        Boş zamanlarımda basketbol maçları izliyorum. NBA ligini takip ediyorum. <br>
+        Aşşağıda NBA takımlarını ve bazı oyuncularını API ile verilerini google sheets'e aktardım.<br><br>
+        <div class="btn-group">
+    <button type="button" class="btn btn-info">Siteler</button>
+    <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">
+      <span class="caret"></span>
+    </button>
+    <ul class="dropdown-menu" role="menu">
+      <li><a href="https://docs.google.com/spreadsheets/d/1pMlp71ahWjjSwqAKN0dWP2NRLpu7AFa7aubwgBGJRx4/edit#gid=1004115727">Oyuncu Listesi</a></li>
+      <li><a href="https://docs.google.com/spreadsheets/d/1pMlp71ahWjjSwqAKN0dWP2NRLpu7AFa7aubwgBGJRx4/edit#gid=2048272483">Batı takımı Listesi</a></li>
+      <li><a href="https://docs.google.com/spreadsheets/d/1pMlp71ahWjjSwqAKN0dWP2NRLpu7AFa7aubwgBGJRx4/edit#gid=0">Doğu takımı Listesi</a></li>
+    </ul>
+  </div>
+</div>
+</div>
+    </div>
+</body>
+</html>
